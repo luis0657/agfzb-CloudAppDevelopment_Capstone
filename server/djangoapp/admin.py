edd4daf88e8ccfd2d9604432bc05e6c929d9a47d
Pablo Garcia <pablo0657@hotmail.com>
2022-08-22 10:20:24 +0200

    models added for Car make and Car model

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarMakeInline(admin.StackedInline):
+    model = CarMake
 # CarModelAdmin class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+
 
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,7 +10,41 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=200, default="Car make")
+    description = models.CharField(max_length=200, default="Description")
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+    
 
+class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default="Car Model")
+    dealerId = models.PositiveIntegerField()
+    year = models.DateField(null=True)
+    SUV = 'SUV'
+    SEDAN = 'Sedan'
+    HB = 'Hatchback'
+    WAGON = 'Wagon'
+    TYPE_CHOICES = [
+        (SUV, 'SUV'),
+        (SEDAN, 'Sedan'),
+        (HB, 'Hatchback'),
+        (WAGON, 'Wagon')
+    ]
+    type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "Car maker: " + self.carmake + \
+            "Model year: " + self.year + \
+            "Dealer Id: " + self.dealerId + \
+            "Type:" + self.type 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
